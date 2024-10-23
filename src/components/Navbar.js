d67afd9c67decae2823f54db8d896bd20a1439a2
Firas Kahlaoui <kahlaouifiras2017@gmail.com>
2024-10-23 21:20:56 +0100

    Add the home button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,46 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "./Navbar.css";
 
 const Navbar = ({ isLoggedIn }) => {
+  const location = useLocation();
+
   return (
     <nav className="navbar">
-      <h1 className="logo">Encryptify</h1>
+      <h1>Encryptify</h1>
       <div className="nav-links">
+        <Link to="/" className={location.pathname === "/" ? "active" : ""}>
+          Home
+        </Link>
         {isLoggedIn ? (
           <>
-            <Link to="/profile">Profile</Link>
-            <Link to="/settings">Settings</Link>
-            <Link to="/logout">Logout</Link>
+            <Link
+              to="/profile"
+              className={location.pathname === "/profile" ? "active" : ""}
+            >
+              Profile
+            </Link>
+            <Link
+              to="/logout"
+              className={location.pathname === "/logout" ? "active" : ""}
+            >
+              Logout
+            </Link>
           </>
         ) : (
           <>
-            <Link to="/login">Login</Link>
-            <Link to="/signup">Sign Up</Link>
+            <Link
+              to="/login"
+              className={location.pathname === "/login" ? "active" : ""}
+            >
+              Login
+            </Link>
+            <Link
+              to="/signup"
+              className={location.pathname === "/signup" ? "active" : ""}
+            >
+              Sign Up
+            </Link>
           </>
         )}
       </div>
